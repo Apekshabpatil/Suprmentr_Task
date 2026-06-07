1786cb7576187f403e7ecf28fa37404146e6d13f
Apekshabpatil <apekshapatil947@gmail.com>
2026-06-07 17:21:02 +0530

    updated

--- data-modeler/test.js
+++ data-modeler/test.js
@@ -1,3 +1,13 @@
 const Blog = require("./blogModel");
 
 console.log("Blog Schema Created Successfully");
+
+const sampleBlog = {
+  title: "Introduction to MERN Stack",
+  content: "Learning MERN Stack Development",
+  author: "Apeksha Patil",
+  category: "Technology",
+  tags: ["MongoDB", "Express", "React", "Node"]
+};
+
+console.log(sampleBlog);
